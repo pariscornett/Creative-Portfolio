abb60e374f765fefbfd0c84af64469e6517f1ddc
Paris Cornett <pariscornett@gmail.com>
2019-06-26 00:09:27 -0500

    added social media icons

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,11 +1,11 @@
-//global variables
 let introOver = false;
 //appends bot icon to DOM
 $(".enter").append("<img id = bot class = valign-center src = assets/images/bot.png>");
 //hide anything you want to fade in and out
 $(".intro").hide();
 $(".name").hide();
-$(".portfolio").hide();
+$(".portfolio").hide()
+$(".button-dump").hide();
 //makes bot clickable, change colors when clicked, and then lead to the rest of portfolio page
 $(".enter").on("click", function(){
     $(".enter").append("<img id = bot2 class = valign-center src = assets/images/bot2.png>");
@@ -51,10 +51,27 @@ $(".enter").on("click", function(){
     console.log(11);
     setTimeout (
         function() {
-            $(".portfolio").fadeOut(1000);
+            $(".portfolio").fadeOut(1000);  
         },18000
     );
-    introOver= true;
+    setTimeout (
+        function() {
+            portfolio();
+        }, 21000
+    )
+   
+    
 })
 
+function portfolio () {
+    $(".button-dump").append("<img id = github class = valign-center src = assets/images/github.png href = https://github.com/pariscornett>");
+    $(".button-dump").fadeIn(1000);
+    $(".button-dump").append("<img id = linkedin class = valign-center src = assets/images/linkedin.png href = https://github.com/pariscornett>");
+    $(".button-dump");
+    $(".button-dump").append("<img id = email class = valign-center src = assets/images/email.png href = # >");
+    $(".button-dump");
+}
 
+$("#github").on("click", function(){
+
+})
