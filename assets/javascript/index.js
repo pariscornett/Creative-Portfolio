807aacc039df93fbc3a406dad04d6efb38cb6611
Paris Cornett <pariscornett@gmail.com>
2019-08-19 13:43:39 -0500

    corrected issue with email image

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -63,13 +63,13 @@ $(".enter").on("click", function(){
 
 function portfolio () {
     $(".button-dump").fadeIn(1000);
-    $(".button-dump").append("<a href=https://github.com/pariscornett><img id=github class=valign-center src=assets/images/github.png></a>");
+    $(".button-dump").append("<a href=https://github.com/pariscornett target=_blank><img id=github class=valign-center src=assets/images/github.png></a>");
     // $(".button-dump").fadeIn(1000);
     // $(".button-dump").append("<img id = linkedin class = valign-center src = assets/images/linkedin.png href = https://github.com/pariscornett>");
     $(".button-dump");
-    $(".button-dump").append("<a href=https://www.linkedin.com/in/paris-cornett-486b63176><img id=linkedin class=valign-center src=assets/images/linkedin.png></a>")
+    $(".button-dump").append("<a href=https://www.linkedin.com/in/paris-cornett-486b63176 target=_blank><img id=linkedin class=valign-center src=assets/images/linkedin.png></a>")
     // $(".button-dump").append("<img id = email class = valign-center src = assets/images/email.png href = # >");
-    $(".button-dump").append("<a href=https://google.com><img id=email class=valign-center src=assets/images/email.png></a>");
+    $(".button-dump").append("<a href=mailto:pariscornett@gmail.com><img id=email class=valign-center src=assets/images/email.png></a>");
 }
 
 $("#github").on("click", function(){
