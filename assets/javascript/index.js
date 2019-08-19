dc168ab982f4f82a67a132b8b375d437a6cf1ee8
Paris Cornett <pariscornett@gmail.com>
2019-08-19 13:17:26 -0500

    cleaned up fadeIns and fadeOuts

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,8 +3,6 @@ let introOver = false;
 $(".enter").append("<img id = bot class = valign-center src = assets/images/bot.png>");
 //hide anything you want to fade in and out
 $(".intro").hide();
-$(".name").hide();
-$(".portfolio").hide()
 $(".button-dump").hide();
 //makes bot clickable, change colors when clicked, and then lead to the rest of portfolio page
 $(".enter").on("click", function(){
@@ -14,7 +12,7 @@ $(".enter").on("click", function(){
     setTimeout(
         function() {
             console.log(2);
-           $(".intro").html("<h1 id = hello>Hello</h1>");  
+           $(".intro").html("<h1 id = welcome>Welcome</h1>");  
            $(".intro").fadeIn(1000);
         },2000
     );
@@ -25,61 +23,62 @@ $(".enter").on("click", function(){
             $(".intro").fadeOut(1000);
         },4000
     );
-    console.log(5);
-    setTimeout (
-        function() {
-            console.log(6);
-            $(".name").html("<p id = name>I'm Paris</p>")
-            $(".name").fadeIn(1000);
-        },6000
-    );
-    console.log(7);
-    setTimeout (
-        function() {
-            console.log(8);
-            $(".name").fadeOut(1000);
-        },8000
-    ); 
-    console.log(9);
-    setTimeout (
-        function() {
-            console.log(10,)
-            $(".portfolio").html("<p id = portfolio>This is My Portfolio</p>");
-            $(".portfolio").fadeIn(1000);
-        },10000
-    );
-    console.log(11);
-    setTimeout (
-        function() {
-            $(".portfolio").fadeOut(1000);  
-        },12000
-    );
+    // console.log(5);
+    // setTimeout (
+    //     function() {
+    //         console.log(6);
+    //         $(".name").html("<p id = name>I'm Paris</p>")
+    //         $(".name").fadeIn(1000);
+    //     },6000
+    // );
+    // console.log(7);
+    // setTimeout (
+    //     function() {
+    //         console.log(8);
+    //         $(".name").fadeOut(1000);
+    //     },8000
+    // ); 
+    // console.log(9);
+    // setTimeout (
+    //     function() {
+    //         console.log(10,)
+    //         $(".portfolio").html("<p id = portfolio>This is My Portfolio</p>");
+    //         $(".portfolio").fadeIn(1000);
+    //     },10000
+    // );
+    // console.log(11);
+    // setTimeout (
+    //     function() {
+    //         $(".portfolio").fadeOut(1000);  
+    //     },12000
+    // );
     setTimeout (
         function() {
             portfolio();
-        }, 14000
+        }, 1000
     )
    
     
 })
 
 function portfolio () {
-    $(".button-dump").append("<img id = github class = valign-center src = assets/images/github.png href = https://github.com/pariscornett>");
     $(".button-dump").fadeIn(1000);
-    $(".button-dump").append("<img id = linkedin class = valign-center src = assets/images/linkedin.png href = https://github.com/pariscornett>");
-    $(".button-dump");
-    $(".button-dump").append("<img id = email class = valign-center src = assets/images/email.png href = # >");
-    $(".button-dump");
+    // $(".button-dump").append("<img id = github class = valign-center src = assets/images/github.png href = https://github.com/pariscornett>");
+    // $(".button-dump").fadeIn(1000);
+    // $(".button-dump").append("<img id = linkedin class = valign-center src = assets/images/linkedin.png href = https://github.com/pariscornett>");
+    // $(".button-dump");
+    // $(".button-dump").append("<img id = email class = valign-center src = assets/images/email.png href = # >");
+    // $(".button-dump");
 }
 
 $("#github").on("click", function(){
-    $("#github").append("<img class = valign-center src = assets/images/github2.png>");
+    $("#github").append("<button><img id = github class = valign-center src = assets/images/github2.png href = https://github.com/pariscornett></button>");
 })
 
-$("#linkedin").on("click", function(){
-    $("#linkedin").append("<img class = valign-center src = assets/images/linkedin2.png>");
-})
+// $("#linkedin").on("click", function(){
+//     $("#linkedin").append("<img id = linkedin class = valign-center src = assets/images/linkedin2.png>");
+// })
 
-$("#email").on("click", function(){
-    $("#email").append("<img class = valign-center src = assets/images/github2.png>");
-})
+// $("#email").on("click", function(){
+//     $("#email").append("<img id = email class = valign-center src = assets/images/github2.png>");
+// })
